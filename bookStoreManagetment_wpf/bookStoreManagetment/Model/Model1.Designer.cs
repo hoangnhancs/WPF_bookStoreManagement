89fa3a797af4fc9a482f403eeb342841703f6864
hoangthang019 <18521394@gm.uit.edu.vn>
2021-11-05 10:12:48 +0700

    fix connect to dtb

--- bookStoreManagetment_wpf/bookStoreManagetment/Model/Model1.Designer.cs
+++ bookStoreManagetment_wpf/bookStoreManagetment/Model/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\University\Nam4_Ky1\NMCNPM\WPF_bookStoreManagement\bookStoreManagetment_wpf\bookStoreManagetment\Model\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\thang\OneDrive\Máy tính\New folder\WPF_bookStoreManagement\bookStoreManagetment_wpf\bookStoreManagetment\Model\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
