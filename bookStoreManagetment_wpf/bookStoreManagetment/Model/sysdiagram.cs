8409e7ca8c78117232091b3b5e3c7be089c8cd03
hoangnhancs <thaihoangnhantk17lqd@gmail.com>
2021-12-05 14:12:16 +0700

    fix38

--- bookStoreManagetment_wpf/bookStoreManagetment/Model/sysdiagram.cs
+++ bookStoreManagetment_wpf/bookStoreManagetment/Model/sysdiagram.cs
@@ -12,15 +12,12 @@ namespace bookStoreManagetment.Model
     using System;
     using System.Collections.Generic;
     
-    public partial class checkItem
+    public partial class sysdiagram
     {
-        public int sttCheckItems { get; set; }
-        public string idCheckItems { get; set; }
-        public string idEmployee { get; set; }
-        public System.DateTime dateCheckItems { get; set; }
-        public string idItem { get; set; }
-        public string note { get; set; }
-        public Nullable<int> newQuantityItem { get; set; }
-        public Nullable<int> oldQuantityItem { get; set; }
+        public string name { get; set; }
+        public int principal_id { get; set; }
+        public int diagram_id { get; set; }
+        public Nullable<int> version { get; set; }
+        public byte[] definition { get; set; }
     }
 }
